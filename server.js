bc1bc593daf329194de93b185d0dfb48fabaacd0
Boban <skrasavcevic@spartagency.com>
2019-11-08 11:38:09 +0100

    test

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/items', itemsRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
+  console.log(111);
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
